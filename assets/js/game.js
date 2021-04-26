051dd4279feeaa08e792f8acf6ca26b4d09e209a
stephgeorge <stephanie.george22@gmail.com>
2021-04-26 17:37:40 -0400

    javascript functions to communicate with the player

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("Tgis is an alert! JavaScript is running");
+var playerName = window.prompt("What is your robot's name");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+//what is this?
+console.log("Our robot's name is  " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// this creates a function names "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
